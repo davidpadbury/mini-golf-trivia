3442e27acb3609e8a0f2e8044c61b840c1b925b6
David Padbury <david@davidpadbury.com>
2022-09-03 07:41:55 -0500

    inital web layout

---
+++ web/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Question">What is 2 + 2?</div>
+      <div className="Answers">
+        <div className="Answer AnswerA">4</div>
+        <div className="Answer AnswerB">Party says 5 🙉</div>
+        <div className="Answer AnswerC">Depends</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
